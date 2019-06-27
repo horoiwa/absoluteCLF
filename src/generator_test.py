001b49818b9a125a15397440a7ca8d31314c0c20
Shinri Horiwa <horoiwa195@gmail.com>
2019-06-28 00:01:46 +0900

    fix generator test

--- src/generator_test.py
+++ src/generator_test.py
@@ -1,4 +1,8 @@
 import os
+import shutil
+
+import numpy as np
+from PIL import Image
 
 from config import BATCH_SIZE, DATA_GEN_ARGS
 from src.generator import customGenerator
@@ -7,14 +11,25 @@ from src.generator import customGenerator
 def generator_test():
     """ 使う時はconfig.pyと同じディレクトリに移動して使用
     """
-    os.makedirs('test_generator')
-    trainGene = customGenerator(batch_size=BATCH_SIZE,
+    dirname = 'image_test'
+    if os.path.exists(dirname):
+        shutil.rmtree(dirname)
+
+    os.makedirs(dirname)
+    trainGene = customGenerator(batch_size=50,
                                 train_path='images',
                                 image_folder='train',
                                 aug_dict=DATA_GEN_ARGS,
-                                save_to_dir='test_generator',
-                                image_color_mode="rgb",)
-    trainGene.__next__()
+                                save_to_dir=None,
+                                image_color_mode="rgb",
+                                inference=False)
+
+    images, labels = trainGene.__next__()
+    for i in range(images.shape[0]):
+        image = images[i, :, :, :] * 255
+        image = Image.fromarray((np.uint8(image)))
+        image.save(f'{dirname}/{i}.png')
+
 
 
 if __name__ == '__main__':
